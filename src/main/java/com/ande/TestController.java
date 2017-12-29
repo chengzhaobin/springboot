80c8608eae801659470d3d0d4e4e90dfe37e0cd3
chengzhaobin <707578903@qq.com>
2017-12-29 16:32:05 +0800

    spring boot 集成 mybatis,druid,pagehelp

--- src/main/java/com/ande/TestController.java
+++ src/main/java/com/ande/TestController.java
@@ -1,20 +1,26 @@
 package com.ande;
 
+
 import javax.annotation.Resource;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import util.PageResult;
+
+import com.ande.model.User;
+import com.ande.service.IUserService;
+
 @RestController
-@RequestMapping("/rr")
 public class TestController {
 	@Resource
-	private Test a;
+	private IUserService userServiceImpl;
 	@RequestMapping("/hello")
-	public String ind(){
-		a.test();
-		return "hello word";
+	public PageResult<User> ind(PageResult<User> page,User user){
+		//PageHelper.startPage(1,2);
+		//List<User> user=userServiceImpl.findUserInfo();
+		userServiceImpl.queryByPage(page, user);
+		return page;
 	}
 }
 
